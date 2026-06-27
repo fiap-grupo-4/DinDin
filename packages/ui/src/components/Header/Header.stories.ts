cedae4ed8e1cc4acff8af8432ec0aaad498b70aa
gusalles <gusalles@hotmail.com>
2026-06-27 10:37:16 -0300

    fix header story

--- packages/ui/src/components/Header/Header.stories.ts
+++ packages/ui/src/components/Header/Header.stories.ts
@@ -1,12 +1,16 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { Header as HeaderComponent } from "./Header";
+import { fn } from "storybook/test";
 
 const meta = {
   title: "Components/Header",
   component: HeaderComponent,
   parameters: {
     layout: "fullscreen",
+  },
+  args: {
+    onToggle: fn(),
   },
   tags: ["autodocs"],
 } satisfies Meta<typeof HeaderComponent>;
